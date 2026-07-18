f87f85891ebc4b37be8dbf00139dbab4c6001e7d
유재진 <yjjn2005@gmail.com>
2026-07-18 20:19:47 +0900

    지도 마커 클릭 완전 수정: tooltip으로 라벨 교체, SW v4 캐시 강제 갱신

--- sw.js
+++ sw.js
@@ -1,23 +1,5 @@
-const CACHE = 'korea-trail-v3';
-const ASSETS = [
-  '/korea-trail-planner/',
-  '/korea-trail-planner/index.html'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
-});
+const CACHE = 'korea-trail-v4';
+const ASSETS = ['/korea-trail-planner/','/korea-trail-planner/index.html'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request)));});
